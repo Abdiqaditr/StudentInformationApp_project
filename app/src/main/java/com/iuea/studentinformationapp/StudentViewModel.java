e76bb4c35bc71d1480572b22ee41d3613eb65702
Abdiqaditr <ashkirj1@gmail.com>
2024-10-27 22:51:14 +0300

    commit 47
    change line 3 i remove unwonted text in the studentVeiw class

--- app/src/main/java/com/iuea/studentinformationapp/StudentViewModel.java
+++ app/src/main/java/com/iuea/studentinformationapp/StudentViewModel.java
@@ -1,6 +1,5 @@
 package com.iuea.studentinformationapp;
 
-// StudentViewModel.java
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
